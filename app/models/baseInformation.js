28d8527ec3376e4f1707d1c5624f9d314f7ac629
mh Amrollahi <mh.amrollahi@gmail.com>
2024-09-09 23:31:57 +0330

    add newBanking_Database.sql

--- app/models/baseInformation.js
+++ app/models/baseInformation.js
@@ -2,7 +2,7 @@ const { executeQuery } = require("@configs/dbconfig");
 
 exports.index = async () => {
   // const _id = 3
-  const query = `SELECT * FROM BG_CodeTableList`;
+  const query = `SELECT * FROM CodeTableList`;
   const result = await executeQuery(query);
   return result.recordset;
 };
