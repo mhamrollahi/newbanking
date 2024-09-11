aacd4e5e3154e0bc8d291c9a466c5a5bac18f8d7
Mohammad Hassan Amrollahi <mh.amrollahi@gmail.com>
2024-09-11 15:07:33 +0330

    add pagination to codeTablelist pages

--- app/controllers/baseInformation.js
+++ app/controllers/baseInformation.js
@@ -1,12 +1,35 @@
-const baseInfoModel = require('@models/baseInformation')
+const baseInfoModel = require("@models/baseInformation");
 
-exports.index = async(req,res,next) => {
+exports.index = async (req, res, next) => {
   try {
-    console.log('in baseInformation Controller ...')
-    const result = await baseInfoModel.index()
+    const page = "page" in req.query ? parseInt(req.query.page) : 1;
+    const perPage = 10;
+    const result = await baseInfoModel.findAll(page, perPage);
+
+    const totalCodeTableLists = await baseInfoModel.count();
+    const totalPages = Math.ceil(totalCodeTableLists / perPage);
+    const pagination = {
+      page,
+      totalPages,
+      nextPage: page < totalPages ? page + 1 : totalPages,
+      prevPage: page > 1 ? page - 1 : 1,
+      isMoreThan3Pages: totalPages > 3,
+      hasNextPage: page < totalPages,
+      hasPrevPage: page > 1,
+    };
+
+    res.render("./baseInformation/index", {
+      layout: "main",
+      result,
+      pagination,
+      helper: {
+        show3Pages_Index: function (is3pages, option) {
+          return is3pages;
+        },
+      },
+    });
     // res.send(result)
-    res.render('./baseInformation/index',{layout:'main',result})
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
--- app/models/baseInformation.js
+++ app/models/baseInformation.js
@@ -6,3 +6,24 @@ exports.index = async () => {
   const result = await executeQuery(query);
   return result.recordset;
 };
+
+exports.findAll = async (page = 1, perPage = 10) => {
+  const offset = (page - 1) * perPage;
+
+  const query = `SELECT * FROM VW_CodeTableList
+    ORDER BY id 
+    OFFSET ${offset} ROWS 
+    FETCH NEXT ${perPage} ROWS ONLY`;
+
+  const result = await executeQuery(query);
+  return result.recordset;
+};
+
+exports.count = async () => {
+  const query = `SELECT COUNT(id) as codeTableListCount FROM VW_CodeTableList`;
+  const result = await executeQuery(query);
+  console.log(result.recordset[0].codeTableListCount);
+  
+  return result.recordset[0].codeTableListCount
+
+};
