3141130e6a89d50a112e75cb0abad57dab43cfe1
Mohammad Hassan Amrollahi <mh.amrollahi@gamil.com>
2024-10-09 14:45:43 +0330

    add CodingData model

--- app/models/baseInformation/codingData.js
+++ app/models/baseInformation/codingData.js
@@ -1,24 +1,116 @@
-const {executeQuery} = require('@configs/dbconfig')
+const sequelize = require("sequelize");
+const { DataTypes } = require("sequelize");
+const dateService = require('@services/dateService')
 
-exports.findAll = async (codeTableListId , page = 1 ,perPage = 10) => { 
-  const offset = (page - 1) * perPage;
-
-  const query = `SELECT * FROM VW_CodingData 
-  WHERE codeTableListId = ${codeTableListId}
-  ORDER BY sortId 
-  OFFSET ${offset} ROWS 
-  FETCH NEXT ${perPage} ROWS ONLY`
-
-  const result= await executeQuery(query)
-  return result.recordset
-  
+exports.test1 = (arg)=>{
+  arg = arg + ' test'
+  console.log('in test1 function ..  ', arg)
 }
 
 
-exports.count = async (id) => {
-  const query = `SELECT COUNT(id) as codingDataCount FROM VW_CodingData WHERE codeTableListId = ${id}`;
-  const result = await executeQuery(query);
-  
-  return result.recordset[0].codingDataCount
+exports.CodingData = (sequelize) => {
+  const CodingData = sequelize.define('CodingData',{
+    codeTableListId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: {
+          args:true,
+          msg: 'کد نمی تواند تکراری باشد.'
+        },
+        validate:{
+          isNumeric: {
+            msg: "کد فقط باید عدد باشد.",
+            }
+          }
+        },
+          
+        title: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+        unique: {
+          args:true,
+          msg: 'نام کدینگ نمی تواند تکراری باشد.'
+          },
+        validate: {
+          notNull:{
+          msg: 'لطفا نام کدینگ جدول را وارد کنید.'
+          },
+        notEmpty:{
+          msg: 'لطفا نام کدینگ جدول را وارد کنید.',
+          },
+        len: {
+          args:[3,50],
+          msg:'نام انگلیسی جدول  باید کلمه‌ای بین 3 تا 50 کاراکتر باشد.'
+          },
+        }
+      },
+        
+      description:{
+        type:DataTypes.STRING(255),
+      },
+        
+      sortId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: {
+          args:true,
+          msg: 'کد نمی تواند تکراری باشد.'
+        },
+        validate:{
+          isNumeric: {
+            msg: "کد فقط باید عدد باشد.",
+            }
+          }
+        },
+          
+        refId: {
+          type: DataTypes.STRING(30),
+          
+          validate:{
+              len:{
+                args:[1,3],
+                msg: 'کد ثانویه  بین 3 تا 30 کاراکتر باشد.'
+                },
+            }
+          },
+            
+        creator:{
+          type:DataTypes.STRING(25),
+          allowNull: false,
+          validate: {
+            notNull:{
+              msg: 'لطفا نام ایجاد کننده را وارد کنید.'
+            },
+          }
+      },
 
-};
+      updater:{
+          type:DataTypes.STRING(25),
+      },
+
+      fa_createdAt:{
+        type:DataTypes.VIRTUAL,
+        get(){
+          const rawValue = this.getDataValue('createdAt')
+          return dateService.toPersianDate(rawValue)
+        },
+
+        fa_updatedAt:{
+        type:DataTypes.VIRTUAL,
+        get(){
+          const rawValue = this.getDataValue('updatedAt')
+          return dateService.toPersianDate(rawValue)
+        },
+       
+      }
+
+      }, {
+        sequelize,
+        validate:{
+          
+      }
+  })
+  // console.log(CodeTableList)
+  return CodingData
+}
+
