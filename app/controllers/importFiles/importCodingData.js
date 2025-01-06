e18865dc4350423b3681e062ba8e777700b2e8e3
mh Amrollahi <mh.amrollahi@gmail.com>
2025-01-06 23:32:29 +0330

    validate date for createdAt in excel Files

--- app/controllers/importFiles/importCodingData.js
+++ app/controllers/importFiles/importCodingData.js
@@ -2,6 +2,7 @@ const xlsx = require("xlsx");
 const { CodingDataModel } = require("../../models");
 const path = require("path");
 const fs = require("fs");
+const moment = require('jalali-moment')
 
 exports.importCodingData = (req, res, next) => {
   try {
@@ -88,7 +89,6 @@ exports.importCodingData_Save = async (req, res, next) => {
         if (error.name === "SequelizeValidationError") {
           errors = error.message.split("Validation error:");
 
-          console.log("errors = ", errors);
           if (errors.length > 0) {
             errorRows.push({
               Row: index + 1,
@@ -102,14 +102,12 @@ exports.importCodingData_Save = async (req, res, next) => {
               Errors: row.Errors,
               ...row.OriginalData,
             }));
-            // req.flash("errors", error.message);
           }
         }
 
         if (error.name === "SequelizeUniqueConstraintError") {
           errors = error.message.split("SequelizeUniqueConstraintError");
 
-          console.log("errors = ", errors);
           if (errors.length > 0) {
             errorRows.push({
               Row: index + 1,
@@ -125,10 +123,10 @@ exports.importCodingData_Save = async (req, res, next) => {
             }));
           }
         }
+
         if (error.name === "SequelizeForeignKeyConstraintError") {
           errors = error.message.split("SequelizeForeignKeyConstraintError");
 
-          console.log("errors = ", errors);
           if (errors.length > 0) {
             errorRows.push({
               Row: index + 1,
@@ -144,37 +142,25 @@ exports.importCodingData_Save = async (req, res, next) => {
             }));
           }
         }
+
       }
 
-
-     
     }
-
-
 
     if (errorSheet) {
       const errorFilePath = createErrorSheet(errorSheet, fileName);
-
       req.flash("errorFilePath", errorFilePath);
-
-      console.log(errorFilePath);
-
-      const filePath = path.join(
-        __dirname,
-        "../../../uploads",
-        req.file.filename
-      );
-      deleteUploadedFile(filePath);
-
-      return res.redirect("/importFiles/importCodingData");
-    } else{
-      req.flash('success','اطلاعات با موفقیت در سامانه ذخیره شد.')
-      return res.redirect('/')
+    } else {
+      req.flash("success", "اطلاعات با موفقیت در سامانه ذخیره شد.");
     }
 
-
+    const filePath = path.join(__dirname,"../../../uploads",req.file.filename);
+    deleteUploadedFile(filePath);
+    return res.redirect("/importFiles/importCodingData");
 
   }
+  
+  
 };
 
 const validateExcelFile = async (file) => {
@@ -230,6 +216,12 @@ const validationRow = (row, index) => {
   if (!row.title) errors.push("عنوان وارد نشده است.");
   if (!row.sortId) errors.push("ترتیب وارد نشده است.");
   if (row.sortId && isNaN(row.sortId)) errors.push("ترتیب باید عدد باشد.");
+  if (!row.createdAt){
+    errors.push('تاریخ ایجاد وارد نشده است')
+  }else{
+    const dateError = validateDate(row.createdAt)
+    if(dateError) errors.push(dateError)    
+  } 
 
   if (errors.length > 0) {
     return {
@@ -241,3 +233,21 @@ const validationRow = (row, index) => {
 
   return null;
 };
+
+
+const validateDate = (date)=>{
+  const allowedFormats = [
+    'YYYY-MM-DD',
+    'YYYY-MM-DD HH:mm:ss.SSS',
+    'YYYY-MM-DD HH:mm:ss',
+    'DD-MM-YYYY'
+  ]
+
+  const isValid = allowedFormats.some((format) =>
+    moment(date,format,true).isValid()
+  )
+
+  if(!isValid) return 'تاریخ باید در فرمت YYYY-MM-DD , YYYY-MM-DD HH:mm:ss.SSS یا YYYY-MM-DD HH:mm:ss'
+
+  return null
+}
