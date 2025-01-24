4063f1d9c66f98462ed5657356ca88834b82ec3a
mh Amrollahi <mh.amrollahi@gmail.com>
2025-01-25 00:17:38 +0330

    add model/admin/user.js

--- app/controllers/admin/users/index.js
+++ app/controllers/admin/users/index.js
@@ -19,3 +19,35 @@ exports.index = async(req,res,next) => {
    next(error) 
   }
 }
+
+exports.create = async (req,res,next)=>{
+  try {
+    const errors = req.flash("errors");
+    const success = req.flash("success");
+    const hasError = errors.length > 0;
+
+    res.render("./admin/user/create", {
+      layout: "main",
+      errors,
+      hasError,
+      success,
+    });
+  } catch (error) {
+    next(error)
+  }
+}
+
+exports.store = async (req,res,next)=>{
+  try {
+    const userData = {
+      userName:'0059935261',
+      password:'Mh@904957',
+      fullName:'Mohammad Hassan Amrollahi',
+      creator:'MHA',
+    }
+    const {id} = 
+  } catch (error) {
+    
+    next(error)
+  }
+}
---
+++ app/models/admin/users/user.js
@@ -0,0 +1,127 @@
+const { DataTypes } = require('sequelize');
+const dateService = require('@services/dateService');
+const hashService = require('@services/hashService')
+
+exports.User = (sequelize) => {
+  const User = sequelize.define(
+    'User',
+    {
+      userName: {
+        type: DataTypes.STRING(11),
+        allowNull: false,
+        unique: {
+          args: true,
+          msg: 'نام کاربری (کد ملی) نمی تواند تکراری باشد.'
+        },
+        validate: {
+          notNull: {
+            msg: 'لطفا نام کاربری (کد ملی) را وارد کنید.'
+          },
+          notEmpty: {
+            msg: 'لطفا نام کاربری (کد ملی) را وارد کنید.'
+          },
+          len: {
+            args: [11, 11],
+            msg: 'نام کاربری همان کدملی هست.لطفا کد ملی خود را وارد کنید.'
+          },
+          isNumeric: {
+            msg: 'کد ملی فقط شامل اعداد می باشد.'
+          }
+        }
+      },
+
+      password: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+        validate: {
+          is: {
+            args: /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-zA-Z]).{6,20}$/, // حداقل یک عدد، یک حرف خاص و یک حرف
+            msg: 'رمز عبور باید حداقل شامل یک عدد، یک کاراکتر خاص (!@#$%^&*) و حروف باشد و حداقل ۶ کاراکتر داشته باشد.'
+          },
+          notNull: {
+            msg: 'لطفا کلمه عبور را وارد کنید.'
+          },
+          notEmpty: {
+            msg: 'لطفا کلمه عبور را وارد کنید.'
+          },
+          len: {
+            args: [6, 20],
+            msg: 'کلمه عبور باید بین 6 تا 20 کاراکتر باشد.'
+          }
+        }
+      },
+
+      fullName: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+        unique: {
+          args: true,
+          msg: 'نام و نام‌خانوادگی نمی تواند تکراری باشد.'
+        },
+        validate: {
+          notNull: {
+            msg: 'لطفا نام و نام‌خانوادگی را وارد کنید.'
+          },
+          notEmpty: {
+            msg: 'لطفا نام و نام‌خانوادگی را وارد کنید.'
+          },
+          len: {
+            args: [5, 50],
+            msg: 'نام و نام‌خانوادگی  باید  بین 5 تا 50 کاراکتر باشد.'
+          }
+        }
+      },
+
+      creator: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'لطفا نام ایجاد کننده را وارد کنید.'
+          }
+        }
+      },
+
+      updatedAt: {
+        type: DataTypes.DATE,
+        default: null
+      },
+
+      updater: {
+        type: DataTypes.STRING(50)
+      },
+
+      fa_createdAt: {
+        type: DataTypes.VIRTUAL,
+        get() {
+          const rawValue = this.getDataValue('createdAt');
+          return dateService.toPersianDate(rawValue);
+        }
+      },
+
+      fa_updatedAt: {
+        type: DataTypes.VIRTUAL,
+        get() {
+          const rawValue = this.getDataValue('updatedAt');
+          return dateService.toPersianDate(rawValue);
+        }
+      }
+    },
+    {
+      timestamps: true,
+      sequelize,
+      validate: {}
+    },
+
+    User.beforeCreate((user)=>{
+      user.password = hashService.hashPassword(user.password)
+    }),
+    // استفاده از هوک برای تنظیم `updatedAt` هنگام بروزرسانی
+    User.beforeUpdate((user) => {
+      user.password = hashService.hashPassword(user.password)
+      user.updatedAt = new Date();
+    })
+  );
+
+  return User;
+};
