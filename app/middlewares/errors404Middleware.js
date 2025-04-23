6d397b63e0e79a68a5effe2c39e2514760662e39
MOhammad Hassan Amrollahi <mh.amrollahi@gmail.com>
2025-04-23 15:20:52 +0330

    fix error 404

--- app/middlewares/errors404Middleware.js
+++ app/middlewares/errors404Middleware.js
@@ -1,7 +1,7 @@
 module.exports = (app) => {
   app.use((req, res, next) => {
     if (!res.headersSent && !req.originalUrl.startsWith('/errors')) {
-      const error = new Error('صفحه مورد نظر یافت نشد');
+      const error = new Error(` صفحه ${req.originalUrl}  یافت نشد`);
       error.status = 404;
       next(error);
     } else {
