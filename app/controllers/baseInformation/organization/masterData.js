f4ac6396060f03c6ac2c785f589de32bf40b5dd6
mh Amrollahi <mh.amrollahi@gmail.com>
2025-05-06 22:41:54 +0330

    fix edit masterdata.js

--- app/controllers/baseInformation/organization/masterData.js
+++ app/controllers/baseInformation/organization/masterData.js
@@ -3,8 +3,8 @@ const { UserViewModel, CodingDataModel, CodeTableListModel, OrganizationMasterDa
 const coding = require('@constants/codingDataTables.js');
 const dateService = require('@services/dateService.js');
 // const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
+// const path = require('path');
+// const fs = require('fs');
 const { organizationSchema  } = require('@validators/organization/masterData');
 const title = 'مدیریت اطلاعات پایه ';
 const subTitle = 'فهرست دستگاه ها ';
@@ -62,7 +62,7 @@ exports.index = async (req, res, next) => {
   }
 };
 
-// نمایش فرم ایجاد سازمان جدید
+// نمایش فرم ایجاد دستگاه جدید
 exports.create = async (req, res, next) => {
   try {
 
@@ -120,12 +120,10 @@ exports.create = async (req, res, next) => {
 };
 
 
-// ذخیره سازمان جدید
+// ذخیره دستگاه جدید
 exports.store = async (req, res, next) => {
   try {
     const { nationalCode, organizationName, registerDate, registerNo, postalCode, address, provinceId, organizationTypeId, organizationCategoryId, description } = req.body;
-    console.log('Register Date before save:', registerDate);
-    // const englishDate = dateService.toEnglishDate(registerDate);
 
     const cleanRegisterDate = !registerDate || registerDate.trim() === '' ? null : registerDate;
 
@@ -159,11 +157,11 @@ exports.store = async (req, res, next) => {
   }
 };
 
-// نمایش فرم ویرایش سازمان
+// نمایش فرم ویرایش دستگاه
 exports.edit = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const organizationData = await OrganizationMasterDataModel.findByPk(id, {
+    const organization = await OrganizationMasterDataModel.findByPk(id, {
       include: [
         {
           model: UserViewModel,
@@ -178,22 +176,24 @@ exports.edit = async (req, res, next) => {
         {
           model: CodingDataModel,
           as: 'province',
-          attributes: ['title']
+          attributes: ['id','title']
         },
         {
           model: CodingDataModel,
           as: 'organizationType',
-          attributes: ['title']
+          attributes: ['id','title']
         },
         {
           model: CodingDataModel,
           as: 'organizationCategory',
-          attributes: ['title']
+          attributes: ['id','title']
         }
-      ]
+      ],
+      raw: true,
+      nest: true
     });
 
-    if (!organizationData) {
+    if (!organization) {
       return res.redirect('/error/404');
     }
     const provincesListData = await CodingDataModel.findAll({
@@ -229,11 +229,14 @@ exports.edit = async (req, res, next) => {
       raw: true,
       nest: true
     });
-
-    res.render('./baseInformation/organization/masterData/edit', {
+    if (organization) {
+      organization.fa_createdAt = dateService.toPersianDate(organization.createdAt);
+      organization.fa_updatedAt = dateService.toPersianDate(organization.updatedAt);
+    }
+    res.adminRender('./baseInformation/organization/masterData/edit', {
       title,
       subTitle,
-      organizationData,
+      organization,
       provincesListData,
       organizationTypesListData,
       organizationCategoriesListData
@@ -243,69 +246,78 @@ exports.edit = async (req, res, next) => {
   }
 };
 
-// بروزرسانی اطلاعات سازمان
+// بروزرسانی اطلاعات دستگاه
 exports.update = async (req, res, next) => {
   try {
     const { id } = req.params;
     const { nationalCode, organizationName, registerDate, registerNo, postalCode, address, provinceId, organizationTypeId, organizationCategoryId, description } = req.body;
   
-    const validationResult = organizationSchema.validate(req.body,{abortEarly: false});
+    // const validationResult = organizationSchema.validate(req.body,{abortEarly: false});
     
-    if (validationResult.error) {
-      req.flash('errors',validationResult.error.details.map((err) => err.message));
-      return res.redirect('./create');
-    }
+    // if (validationResult.error) {
+    //   req.flash('errors',validationResult.error.details.map((err) => err.message));
+    //   return res.redirect(`../edit/${id}`);
+    // }
 
     const organization = await OrganizationMasterDataModel.findByPk(id);
     if (!organization) {
-      return res.status(404).send('سازمان مورد نظر یافت نشد');
+      return res.status(404).send('دستگاه مورد نظر یافت نشد');
     }
+    const cleanRegisterDate = !registerDate || registerDate.trim() === '' ? null : registerDate;
 
     // آپلود فایل‌های جدید
-    const files = {
-      filePathStatute: req.files?.filePathStatute?.[0],
-      filePathFinancial: req.files?.filePathFinancial?.[0],
-      filePathFoundationAd: req.files?.filePathFoundationAd?.[0]
-    };
+    // const files = {
+    //   filePathStatute: req.files?.filePathStatute?.[0],
+    //   filePathFinancial: req.files?.filePathFinancial?.[0],
+    //   filePathFoundationAd: req.files?.filePathFoundationAd?.[0]
+    // };
 
-    const filePaths = {};
-    for (const [key, file] of Object.entries(files)) {
-      if (file) {
-        // حذف فایل قدیم
-        if (organization[key]) {
-          fs.unlinkSync(organization[key]);
-        }
+    // const filePaths = {};
+    // for (const [key, file] of Object.entries(files)) {
+    //   if (file) {
+    //     // حذف فایل قدیم
+    //     if (organization[key]) {
+    //       fs.unlinkSync(organization[key]);
+    //     }
 
-        // ذخیره فایل جدید
-        const fileName = `${Date.now()}-${file.originalname}`;
-        const filePath = path.join('uploads', 'organizations', fileName);
-        fs.writeFileSync(filePath, file.buffer);
-        filePaths[key] = filePath;
-      }
-    }
+    //     // ذخیره فایل جدید
+    //     const fileName = `${Date.now()}-${file.originalname}`;
+    //     const filePath = path.join('uploads', 'organizations', fileName);
+    //     fs.writeFileSync(filePath, file.buffer);
+    //     filePaths[key] = filePath;
+    //   }
+    // }
 
-    await organization.update({
+    const rowsAffected = await organization.update({
       nationalCode,
       organizationName,
-      registerDate,
-      registerNo,
-      postalCode,
-      address,
-      provinceId,
-      organizationTypeId,
-      organizationCategoryId,
+      registerDate: cleanRegisterDate,
+      registerNo: registerNo == '' ? null : registerNo,
+      postalCode: postalCode == '' ? null : postalCode,
+      address: address == '' ? null : address,
+      provinceId: provinceId == '' ? null : provinceId,
+      organizationTypeId: organizationTypeId == '' ? null : organizationTypeId,
+      organizationCategoryId: organizationCategoryId == '' ? null : organizationCategoryId,
       description,
-      ...filePaths,
-      updaterId: req.user.id
-    });
+      updaterId: req.session.user.id
+    },
+    { where: { id }, individualHooks: true }
+  );
 
-    res.redirect('./index');
+    if (rowsAffected) {
+      req.flash('success', 'اطلاعات با موفقیت اصلاح شد.');
+      return res.redirect('../index');
+    }
+
+    req.flash('errors', 'اصلاح اطلاعات با مشکل مواجه شد . لطفا مجددا سعی کنید...');
+
+    return res.redirect(`../edit/${id}`);
   } catch (error) {
     next(error);
   }
 };
 
-// حذف سازمان
+// حذف دستگاه
 exports.delete = async (req, res, next) => {
   try {
     const { id } = req.params;
