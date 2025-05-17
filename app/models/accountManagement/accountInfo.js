8928c204f89733cd6b21af78f92bb3c179bff4ed
MOhammad Hassan Amrollahi <mh.amrollahi@gmail.com>
2025-05-17 13:49:45 +0330

    add accountInfo model and create.handlebars

--- app/models/accountManagement/accountInfo.js
+++ app/models/accountManagement/accountInfo.js
@@ -1,6 +1,5 @@
 const { DataTypes } = require('sequelize');
 const BaseModel = require('@models/baseModel');
-const dateService = require('@services/dateService');
 
 class AccountInfo  extends BaseModel {}
 
@@ -8,7 +7,6 @@ module.exports = (sequelize) => {
   AccountInfo.init(
     {
 
-      // فیلدهای جدید
       accountNumber: {
         type: DataTypes.STRING(20),
         allowNull: false,
@@ -117,7 +115,7 @@ module.exports = (sequelize) => {
       accountTypeId: {
         type: DataTypes.INTEGER,
         references: {
-          model: '  ',
+          model: 'codingdata',
           key: 'id'
         },
         onUpdate: 'RESTRICT',
@@ -130,270 +128,41 @@ module.exports = (sequelize) => {
           model: 'bankBranch',
           key: 'id'
         },
-      },
-      obstructStatus: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: false
-      },
-      // فیلدهای قدیمی
-      nationalCode: {
-        type: DataTypes.STRING(11),
-        allowNull: false,
-        validate: {
-          notNull: {
-            msg: 'لطفا  شناسه ملی را وارد کنید.'
-          },
-          notEmpty: {
-            msg: 'لطفا  شناسه ملی را وارد کنید.'
-          },
-          len: {
-            args: [11, 11],
-            msg: ' شناسه ملی باید بین 0 تا ۲۰ حرف باشد.'
-          },
-          isNumericOrPersian(value) {
-            // تبدیل اعداد فارسی به انگلیسی
-            const persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g];
-            const englishNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-            let convertedValue = value;
-            for (let i = 0; i < 10; i++) {
-              convertedValue = convertedValue.replace(persianNumbers[i], englishNumbers[i]);
-            }
-
-            // بررسی اینکه آیا همه کاراکترها عدد هستند
-            if (!/^\d+$/.test(convertedValue)) {
-              throw new Error('شناسه ملی  باید فقط شامل اعداد باشد 111.');
-            }
-          }
-        }
-      },
-
-      organizationName: {
-        type: DataTypes.STRING(200),
-        allowNull: false,
-        validate: {
-          notNull: {
-            msg: 'لطفا عنوان دستگاه را وارد کنید.'
-          },
-          notEmpty: {
-            msg: 'لطفا عنوان دستگاه را وارد کنید.'
-          },
-          len: {
-            args: [1, 200],
-            msg: 'عنوان دستگاه باید بین 1 تا 200 حرف باشد.'
-          }
-        }
-      },
-
-      budgetRow: {
-        type: DataTypes.STRING(8),
-        allowNull: false,
-        validate: {
-          notNull: {
-            msg: 'لطفا  ردیف بودجه را وارد کنید.'
-          },
-          notEmpty: {
-            msg: 'لطفا  ردیف بودجه را وارد کنید.'
-          },
-          len: {
-            args: [8, 8],
-            msg: 'ردیف بودجه باید 8 رقم باشد.'
-          },
-          isNumericOrPersian(value) {
-            // تبدیل اعداد فارسی به انگلیسی
-            const persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g];
-            const englishNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-            let convertedValue = value;
-            for (let i = 0; i < 10; i++) {
-              convertedValue = convertedValue.replace(persianNumbers[i], englishNumbers[i]);
-            }
-
-            // بررسی اینکه آیا همه کاراکترها عدد هستند
-            if (!/^\d+$/.test(convertedValue)) {
-              throw new Error('شناسه ملی  باید فقط شامل اعداد باشد 111.');
-            }
-          }
-        }
-      },
-
-      registerDate: {
-        type: DataTypes.DATE,
-        get() {
-          const rawValue = this.getDataValue('registerDate');
-          if (!rawValue) return null;
-          // Convert Date to string and format it as YYYY/MM/DD
-          const dateStr = rawValue.toISOString().split('T')[0];
-          return dateStr.replace(/-/g, '/');
-        },
-        set(value) {
-          console.log('Setting date value:', value);
-          if (!value) {
-            this.setDataValue('registerDate', null);
-            return;
-          }
-          const gregorianDate = dateService.toEnglishDate(value);
-          console.log('Converted to Gregorian:', gregorianDate);
-          this.setDataValue('registerDate', gregorianDate);
-        }
-      },
-
-      registerNo: {
-        type: DataTypes.STRING(10),
-        validate: {
-          len: {
-            args: [0, 10],
-            msg: 'شماره ثبت باید بین 0 تا 10 حرف باشد.'
-          },
-
-          isNumericOrPersian(value) {
-            // تبدیل اعداد فارسی به انگلیسی
-            const persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g];
-            const englishNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-            let convertedValue = value;
-            for (let i = 0; i < 10; i++) {
-              convertedValue = convertedValue.replace(persianNumbers[i], englishNumbers[i]);
-            }
-
-            // بررسی اینکه آیا همه کاراکترها عدد هستند
-            if (!/^\d+$/.test(convertedValue)) {
-              throw new Error('شماره ثبت باید فقط شامل اعداد باشد 111.');
-            }
-          }
-        }
-      },
-
-      postalCode: {
-        type: DataTypes.STRING(10),
-        validate: {
-          len: {
-            args: [0, 10],
-            msg: 'کد پستی باید بین 10 عدد باشد.'
-          },
-          isNumericOrPersian(value) {
-            if (!value || value.trim() === '') return; // اگر مقدار خالی باشد، اجازه می‌دهیم
-            // تبدیل اعداد فارسی به انگلیسی
-            const persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g];
-            const englishNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-            let convertedValue = value;
-            for (let i = 0; i < 10; i++) {
-              convertedValue = convertedValue.replace(persianNumbers[i], englishNumbers[i]);
-            }
-
-            // بررسی اینکه آیا همه کاراکترها عدد هستند
-            if (!/^\d+$/.test(convertedValue)) {
-              throw new Error('شماره تلفن باید فقط شامل اعداد باشد.');
-            }
-          }
-        }
-      },
-
-      address: {
-        type: DataTypes.STRING(200),
-        validate: {
-          len: {
-            args: [0, 200],
-            msg: 'آدرس دستگاه باید بین 0 تا 200 حرف باشد.'
-          }
-        }
+        onUpdate: 'RESTRICT',
+        onDelete: 'RESTRICT'
       },
 
       provinceId: {
         type: DataTypes.INTEGER,
         references: {
-          model: 'codingdata',
+          model: 'province',
           key: 'id'
         },
         onUpdate: 'RESTRICT',
         onDelete: 'RESTRICT'
       },
 
-
-      //جنس دستگاه
-      organizationTypeId: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'codingdata',
-          key: 'id'
-        },
-        onUpdate: 'RESTRICT',
-        onDelete: 'RESTRICT'
+      // وضعیت حساب: مسدود / غیرمسدود
+      obstructStatus: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+      
+      // وضعیت حساب: باز / بسته
+      accountStatus: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+      
+      requestObstructNo: {
+        type: DataTypes.STRING(20),
       },
 
-      //نوع دستگاه
-      organizationCategoryId: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'codingdata',
-          key: 'id'
-        },
-        onUpdate: 'RESTRICT',
-        onDelete: 'RESTRICT'
+      requestObstructDate: {
+        type: DataTypes.DATE,
       },
-
-      // filePathStatute: {
-      //   type: DataTypes.BLOB,
-      //   validate: {
-      //     isFileSizeValid(value) {
-      //       if (value && value.length > 5 * 1024 * 1024) {
-      //         // 5MB
-      //         throw new Error('حجم فایل اساسنامه نباید بیشتر از 5 مگابایت باشد.');
-      //       }
-      //     },
-      //     isFileTypeValid(value) {
-      //       if (value) {
-      //         // بررسی نوع فایل (مثلاً فقط PDF)
-      //         const allowedTypes = ['application/pdf'];
-      //         if (!allowedTypes.includes(value.type)) {
-      //           throw new Error('فایل اساسنامه باید از نوع PDF باشد.');
-      //         }
-      //       }
-      //     }
-      //   }
-      // },
-
-      // filePathFinancial: {
-      //   type: DataTypes.BLOB,
-      //   validate: {
-      //     isFileSizeValid(value) {
-      //       if (value && value.length > 5 * 1024 * 1024) {
-      //         // 5MB
-      //         throw new Error('حجم فایل مالی نباید بیشتر از 5 مگابایت باشد.');
-      //       }
-      //     },
-      //     isFileTypeValid(value) {
-      //       if (value) {
-      //         const allowedTypes = ['application/pdf'];
-      //         if (!allowedTypes.includes(value.type)) {
-      //           throw new Error('فایل مالی باید از نوع PDF باشد.');
-      //         }
-      //       }
-      //     }
-      //   }
-      // },
-
-      // filePathFoundationAd: {
-      //   type: DataTypes.BLOB,
-      //   validate: {
-      //     isFileSizeValid(value) {
-      //       if (value && value.length > 5 * 1024 * 1024) {
-      //         // 5MB
-      //         throw new Error('حجم فایل آگهی تأسیس نباید بیشتر از 5 مگابایت باشد.');
-      //       }
-      //     },
-      //     isFileTypeValid(value) {
-      //       if (value) {
-      //         const allowedTypes = ['application/pdf'];
-      //         if (!allowedTypes.includes(value.type)) {
-      //           throw new Error('فایل آگهی تأسیس باید از نوع PDF باشد.');
-      //         }
-      //       }
-      //     }
-      //   }
-      // },
-
-      // isConfirmed: {
-      //   type: DataTypes.BOOLEAN
-      // },
 
       description: {
         type: DataTypes.TEXT,
@@ -404,6 +173,7 @@ module.exports = (sequelize) => {
           }
         }
       }
+
     },
     {
       timestamps: true,
@@ -424,14 +194,14 @@ module.exports = (sequelize) => {
   AccountInfo.sequelize = sequelize;
 
   AccountInfo.associate = (models) => {
-    AccountInfo.belongsTo(models.CodingDataModel, { foreignKey: 'provinceId', as: 'province' });
-    OrganizationMasterData.belongsTo(models.CodingDataModel, { foreignKey: 'organizationTypeId', as: 'organizationType' });
-    OrganizationMasterData.belongsTo(models.CodingDataModel, { foreignKey: 'organizationCategoryId', as: 'organizationCategory' });
-    OrganizationMasterData.belongsTo(models.OrganizationMasterDataModel, { foreignKey: 'parentOrganizationId', as: 'parentOrganization' });
+    AccountInfo.belongsTo(models.CodingDataModel, { foreignKey: 'accountTypeId', as: 'accountType' });
+    AccountInfo.belongsTo(models.BankBranchModel , { foreignKey: 'bankBranchId', as: 'bankBranch' });
+    AccountInfo.belongsTo(models.CodeOnlineModel , { foreignKey: 'codeOnlineId', as: 'codeOnline' });
+    AccountInfo.belongsTo(models.OrganizationMasterDataModel, { foreignKey: 'organizationId', as: 'organization' });
     
     
-    OrganizationMasterData.belongsTo(models.UserViewModel, { foreignKey: 'creatorId', as: 'creator' });
-    OrganizationMasterData.belongsTo(models.UserViewModel, { foreignKey: 'updaterId', as: 'updater' });
+    AccountInfo.belongsTo(models.UserViewModel, { foreignKey: 'creatorId', as: 'creator' });
+    AccountInfo.belongsTo(models.UserViewModel, { foreignKey: 'updaterId', as: 'updater' });
 
 
 
